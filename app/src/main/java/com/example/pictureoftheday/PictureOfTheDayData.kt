bc8dc29cd5311d4282ca73281c4cb282a64161a9
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-04-18 12:45:18 +0300

    some refactoring

--- app/src/main/java/com/example/pictureoftheday/PictureOfTheDayData.kt
+++ app/src/main/java/com/example/pictureoftheday/PictureOfTheDayData.kt
@@ -3,6 +3,5 @@ package com.example.pictureoftheday
 sealed class PictureOfTheDayData {
     data class Success(val serverData: PODServerResponseData) : PictureOfTheDayData()
     data class Error(val error: Throwable) : PictureOfTheDayData()
-    data class Loading(val progress: Int) : PictureOfTheDayData()
-
+    data class Loading(val progress: Int?) : PictureOfTheDayData()
 }
