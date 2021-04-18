b8aa4ece255963d4a02a19026c094d93672344cb
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-04-18 12:44:54 +0300

    changing MainFragment class

--- app/src/main/java/com/example/pictureoftheday/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/pictureoftheday/ui/main/MainFragment.kt
@@ -1,30 +1,77 @@
 package com.example.pictureoftheday.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import coil.api.load
+import com.example.pictureoftheday.PictureOfTheDayData
 import com.example.pictureoftheday.R
+import com.example.pictureoftheday.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.getData().observe(viewLifecycleOwner, { renderData(it) })
+    }
+
+    private fun renderData(data: PictureOfTheDayData?) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    toast("Link is empty")
+                } else {
+                    binding.imageView.load(url) {
+                        lifecycle(this@MainFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+
+            }
+            is PictureOfTheDayData.Error -> {
+                toast(data.error.message)
+            }
+        }
+    }
+
+    private fun Fragment.toast(string: String?) {
+        Toast.makeText(context, string, Toast.LENGTH_LONG).apply {
+            setGravity(Gravity.BOTTOM, 0, 250)
+            show()
+        }
     }
 
 }
