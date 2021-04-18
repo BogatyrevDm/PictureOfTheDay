4f8af1bdaf84e99bcf020c855f356a85fe127415
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-04-18 11:13:01 +0300

    creating EquilateralImageView class

---
+++ app/src/main/java/com/example/pictureoftheday/EquilateralImageView.kt
@@ -0,0 +1,15 @@
+package com.example.pictureoftheday
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class EquilateralImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
